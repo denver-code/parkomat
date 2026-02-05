645aebcb7d5ad0bb95067883a4124cec26f5bce6
Ihor Savenko <csigorek@gmail.com>
2026-02-05 20:49:13 +0000

    change logout endrpoint

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -9,7 +9,7 @@ export default function LogoutPage() {
   useEffect(() => {
     async function logout() {
       await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/private/auth/logout`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/public/auth/logout`,
         {
           method: "POST",
           credentials: "include",
